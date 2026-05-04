8ff2589fe8478e985f913319da6e2bffd9881b01
Marcellino Pinem <marcellpinem@gmail.com>
2026-05-04 09:49:16 +0700

    vercel.json

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["10.60.53.26"],
+  devIndicators: false,
 };
 
 export default nextConfig;
